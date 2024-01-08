2ed2d61d4a7a9a78b90e74798a451d8862a6be3d
AnayochukwuJames <anayochukwujames11@gmail.com>
2024-01-08 23:53:47 +0100

    update initial commit

--- src/test/java/com/example/xpresspaymentapi/XpressPaymentApiApplicationTests.java
+++ src/test/java/com/example/xpresspaymentapi/XpressPaymentApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class XpressPaymentApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }

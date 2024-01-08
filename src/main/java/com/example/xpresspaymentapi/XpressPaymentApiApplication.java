aa1b0fca84b8a6efe181e7dade7d9b996800fb4d
AnayochukwuJames <anayochukwujames11@gmail.com>
2024-01-08 22:52:05 +0100

    First commit

---
+++ src/main/java/com/example/xpresspaymentapi/XpressPaymentApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.xpresspaymentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpressPaymentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XpressPaymentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/xpresspaymentapi/XpressPaymentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xpresspaymentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpressPaymentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
